56d4ad67693a36b424f1ffcdd68959ee7bc3e20b
Marco A. <161896294+MarcoALR@users.noreply.github.com>
2025-06-14 09:17:54 -0300

    🎈

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import { useState, useRef } from "react";
+import axios from "axios";
+import "./App.css";
+import WeatherInformations from "./components/WeatherInformations/WeatherInformations";
+import WeatherInformations5Days from "./components/WeatherInformations5days/WeatherInformations5days";
+
+function App() {
+  const [weather, setWeather] = useState();
+  const [weather5Days, setWeather5Days] = useState();
+  const [localTime, setLocalTime] = useState("");
+  const [rainChance, setRainChance] = useState(null);
+  const inputRef = useRef();
+
+  async function searchLocalTimeAndRain(lat, lon) {
+    const key = "38cde37e61fe4ce99a8225813251306"; // WeatherAPI
+    const url = `https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${lat},${lon}&days=1&lang=pt`;
+
+    try {
+      const response = await axios.get(url);
+      const time = response.data.location.localtime;
+      const hourOnly = time.split(" ")[1];
+      const rain = response.data.forecast.forecastday[0].day.daily_chance_of_rain;
+
+      setLocalTime(hourOnly);
+      setRainChance(rain);
+    } catch (error) {
+      console.error("Erro ao buscar hora local ou chance de chuva:", error);
+      setLocalTime("Indisponível");
+      setRainChance(null);
+    }
+  }
+
+  async function searchCity() {
+    const city = inputRef.current.value;
+    const key = "610532f197f9ff53d5b683d790f1fc31"; // OpenWeatherMap API
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`;
+    const url5days = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}&lang=pt_br&units=metric`
+
+const apiinfo5days = await axios.get(url5days)
+
+console.log(url5days)
+try {
+    const apiinfo5days = await axios.get(url5days);
+    setWeather5Days(apiinfo5days.data);
+
+    const apiInfo = await axios.get(url);
+    setWeather(apiInfo.data);
+
+    const { lat, lon } = apiInfo.data.coord;
+    await searchLocalTimeAndRain(lat, lon);
+
+  } catch (error) {
+    console.error("Erro ao buscar dados da cidade:", error);
+    alert("Erro ao buscar dados da cidade.");
+  }
+}
+
+  return (
+    <div className="conteiner">
+      <h1>Previsão do tempo</h1>
+      <input ref={inputRef} type="text" placeholder="Digite o nome da sua cidade" />
+      <button className="botao-laranja" onClick={searchCity}>Buscar</button>
+      {weather && <WeatherInformations weather={weather} localTime={localTime} rainChance={rainChance}/>}
+      {weather5Days && <WeatherInformations5Days weather5Days={weather5Days}/>}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/WeatherInformations/WeatherInformations.jsx
@@ -0,0 +1,57 @@
+import './WeatherInformations.css';
+
+function WeatherInformations({ weather, localTime, rainChance }) {
+  if (!weather || !weather.main || !weather.weather || !weather.wind) {
+    return <p>Dados indisponíveis.</p>;
+  }
+  const temperature = Math.round(weather.main.temp);
+  const feelsLike = Math.round(weather.main.feels_like);
+  const humidity = weather.main.humidity;
+  const windSpeed = (weather.wind.speed * 3.6).toFixed(1);
+  const windDeg = weather.wind.deg;
+  const cityName = weather.name;
+
+  const getWindDirection = (deg) => {
+    const directions = [
+      'Norte', 'Nordeste', 'Leste', 'Sudeste',
+      'Sul', 'Sudoeste', 'Oeste', 'Noroeste'
+    ];
+    const index = Math.round(deg / 45) % 8;
+    return directions[index];
+  };
+
+  const windDirectionText = getWindDirection(windDeg);
+
+  const rainVolume = weather.rain?.['1h'] || weather.rain?.['3h'] || 0;
+  const rainText = rainVolume > 0 ? `${rainVolume} mm` : 'Sem chuva recente';
+
+  
+  const estaChovendoAgora =
+    rainVolume > 0 || weather.weather[0].main.toLowerCase().includes("rain");
+
+  return (
+    <div className="weather-container">
+      <h2>{cityName}</h2>
+      <p className="hora-local">🕒 {localTime}</p>
+      <div className="weather-info">
+        <img
+          src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}style={{ filter: "hue-rotate(47deg) saturate(1300%) brightness(1.05)"}}alt={`Ícone representando clima: ${weather.weather[0].description}`}/>
+        <p className="temperature">{temperature}°C</p>
+      </div>
+
+      <p className="description">Clima: {weather.weather[0].description}</p>
+
+      <div className="details">
+        <p>Sensação Térmica: {feelsLike}°C</p>
+        <p>Direção do Vento: {windDirectionText} ({windDeg}°)</p>
+        <p>Umidade: {humidity}%</p>
+        <p>Volume de Chuva: {rainText}</p>
+        <p>Chance de Chuva (previsão): {rainChance !== null ? `${rainChance}%` : 'Indisponível'}</p>
+        {estaChovendoAgora && (<p className='estachovendo'style={{ color: '#4169E1', fontWeight: 'bold' }}  >Está chovendo agora ☔</p>)}
+        <p>Vento: {windSpeed} km/h</p>
+      </div>
+    </div>
+  );
+}
+
+export default WeatherInformations;
